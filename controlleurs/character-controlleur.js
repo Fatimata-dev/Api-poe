a2be126137193db12429ca651ff623250e01188f
Fatimata-dev <dramefatim7@gmail.com>
2021-07-29 09:21:08 +0200

    projet nodjs

--- controlleurs/character-controlleur.js
+++ controlleurs/character-controlleur.js
@@ -7,9 +7,21 @@ class CharacterControlleur {
         res.json(data);
     }
 
-    getOne = () =>{}
+    getOne = async (req, res) =>{
+        const id = req.params.id;
+        const data = await Character.findByPk(id);
+        res.json(data);
+    }
 
-    create = () =>{}
+    create = async (req,res) =>{
+        const new_character = {
+            name:req.body.name,
+            xp:req.body.xp || 0,
+            pv:req.body.pv || 20,
+        }
+        const data = await Character.create(new_character);
+        res.json(data);
+    }
 
     update = () =>{}
 
--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const Characters = require('./models/character')
 
 const app = expres();
 const port = 3000;
-const CharacterControlleur = require('./controlleurs/character-controlleur')
+const CharacterControlleur = require('./controlleurs/character-controlleur');
+app.use(expres.json());
 app.get('/', (req, res)=> res.json({message : 'Bonjour le jour'}));
 
-app.get('Characters',(req,res)=>{
+app.get('Character',(req,res)=>{
     res.json(Characters.findAll())
 })
-app.get('/characters',  CharacterControlleur.getAll)
+
+app.get('/character',  CharacterControlleur.getAll)
+app.get('/character/:id',  CharacterControlleur.getOne)
+app.post('/character',  CharacterControlleur.create)
 app.listen(port, ()  => {
     console.log('Server is running on port ', port);
 })
